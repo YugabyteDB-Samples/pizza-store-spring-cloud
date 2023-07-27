3489df269a33191d180e1a3d12b37bd3908cda58
Denis Magda <dmagda@gridgain.com>
2023-07-27 12:30:53 -0400

    Added skeleton for the pizza store

---
+++ tracker/src/main/java/com/pizza/tracker/TrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pizza.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerApplication.class, args);
+	}
+
+}
